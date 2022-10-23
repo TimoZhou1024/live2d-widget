bfdcf528bb4fa3e70baef39a9123f190e3feaaad
Timo <66362649+TimoZhou1024@users.noreply.github.com>
2022-10-23 20:55:38 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			apiPath: "https://live2d.fghrsh.net/api/",
+			apiPath: "https://www.timozhou.top:10013/live2d/",
 			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
